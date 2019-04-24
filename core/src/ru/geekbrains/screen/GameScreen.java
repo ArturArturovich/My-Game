6462483512d086a0f7697b467bcad19eb33167a6
ArturArturovich <40322011+ArturArturovich@users.noreply.github.com>
2019-04-24 17:46:00 +0300

    Create game space shooter

---
+++ core/src/ru/geekbrains/screen/GameScreen.java
@@ -0,0 +1,131 @@
+package ru.geekbrains.screen;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.Vector2;
+
+import ru.geekbrains.base.Base2DScreen;
+import ru.geekbrains.math.Rect;
+import ru.geekbrains.pool.BulletPool;
+import ru.geekbrains.sprite.Background;
+import ru.geekbrains.sprite.MainShip;
+import ru.geekbrains.sprite.Star;
+
+public class GameScreen extends Base2DScreen {
+
+    private static final int STAR_COUNT = 64;
+
+    private Texture bg;
+    private TextureAtlas textureAtlas;
+
+    private Background background;
+
+    private Star[] star;
+
+    private MainShip mainShip;
+
+    private BulletPool bulletPool;
+
+    public GameScreen(Game game) {
+        super(game);
+    }
+
+    @Override
+    public void show() {
+        super.show();
+        textureAtlas = new TextureAtlas("textures/mainAtlas.tpack");
+        bg = new Texture("textures/bg.png");
+        background = new Background(new TextureRegion(bg));
+        star = new Star[STAR_COUNT];
+        for (int i = 0; i < star.length; i++) {
+            star[i] = new Star(textureAtlas);
+        }
+        bulletPool = new BulletPool();
+        mainShip = new MainShip(textureAtlas, bulletPool);
+    }
+
+    @Override
+    public void render(float delta) {
+        update(delta);
+        checkCollisions();
+        deleteAllDestroyed();
+        draw();
+    }
+
+    public void update(float delta) {
+        for (int i = 0; i < star.length; i++) {
+            star[i].update(delta);
+        }
+        mainShip.update(delta);
+        bulletPool.updateActiveSprites(delta);
+    }
+
+    public void checkCollisions() {
+
+    }
+
+    public void deleteAllDestroyed() {
+        bulletPool.freeAllDestroyedActiveSprites();
+    }
+
+    public void draw() {
+        Gdx.gl.glClearColor(1, 0.3f, 0.6f, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        batch.begin();
+        background.draw(batch);
+        for (int i = 0; i < star.length; i++) {
+            star[i].draw(batch);
+        }
+        mainShip.draw(batch);
+        bulletPool.drawActiveSprites(batch);
+        batch.end();
+    }
+
+    @Override
+    public void resize(Rect worldBounds) {
+        super.resize(worldBounds);
+        background.resize(worldBounds);
+        for (int i = 0; i < star.length; i++) {
+            star[i].resize(worldBounds);
+        }
+        mainShip.resize(worldBounds);
+    }
+
+    @Override
+    public void dispose() {
+        bg.dispose();
+        textureAtlas.dispose();
+        bulletPool.dispose();
+        super.dispose();
+    }
+
+    @Override
+    public boolean touchDown(Vector2 touch, int pointer) {
+        mainShip.touchDown(touch, pointer);
+        return super.touchDown(touch, pointer);
+    }
+
+    @Override
+    public boolean touchUp(Vector2 touch, int pointer) {
+        mainShip.touchUp(touch, pointer);
+        return super.touchUp(touch, pointer);
+    }
+
+    @Override
+    public boolean keyDown(int keycode) {
+        mainShip.keyDown(keycode);
+        return super.keyDown(keycode);
+    }
+
+    @Override
+    public boolean keyUp(int keycode) {
+        mainShip.keyUp(keycode);
+        return super.keyUp(keycode);
+    }
+
+}
